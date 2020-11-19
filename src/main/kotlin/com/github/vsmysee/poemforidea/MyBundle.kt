d685a56b351adff22683bd3645ab9371646dc926
GitHub Action <action@github.com>
2020-11-19 12:41:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/vsmysee/poemforidea/MyBundle.kt
+++ src/main/kotlin/com/github/vsmysee/poemforidea/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.vsmysee.poemforidea
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/vsmysee/poemforidea/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/vsmysee/poemforidea/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.vsmysee.poemforidea.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vsmysee.poemforidea.MyBundle
 
 class MyApplicationService {
 
