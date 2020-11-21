115eb7b033d271a4e5a9bb85aa38413919995279
codingbaby <vsmysee@gmail.com>
2020-11-21 10:24:43 +0800

    update

--- src/main/java/com/github/vsmysee/poemforidea/BackAction.java
+++ src/main/java/com/github/vsmysee/poemforidea/BackAction.java
@@ -4,10 +4,36 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import org.jetbrains.annotations.NotNull;
 
+import javax.swing.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
 public class BackAction extends AnAction {
+
+    private static Stack<String> stack = new Stack<>();
+
+    public static void add(String poem) {
+        stack.push(poem);
+    }
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
 
+        JPanel holder = PoemToolWindowFactory.holder;
+        holder.remove(0);
+        String item = PoemToolWindowFactory.random();
+        try {
+            item = stack.pop();
+            if (item.equals(ForwardAction.current)) {
+                item = stack.pop();
+            }
+        } catch (Exception exception) {
+        }
+
+        List<String> poems = Arrays.asList(item.split(";"));
+
+        holder.add(PoemBuilder.build(poems).getPoem());
+        holder.updateUI();
     }
 }
--- src/main/java/com/github/vsmysee/poemforidea/ForwardAction.java
+++ src/main/java/com/github/vsmysee/poemforidea/ForwardAction.java
@@ -10,15 +10,22 @@ import java.util.List;
 
 public class ForwardAction extends AnAction {
 
+    public static String current;
+
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
 
         JPanel holder = PoemToolWindowFactory.holder;
         holder.remove(0);
-        List<String> poems = Arrays.asList(PoemToolWindowFactory.random().split(";"));
+        String random = PoemToolWindowFactory.random();
+        current = random;
+
+        BackAction.add(random);
+
+        List<String> poems = Arrays.asList(random.split(";"));
 
         holder.add(PoemBuilder.build(poems).getPoem());
-        holder.getParent().repaint();
+        holder.updateUI();
 
     }
 }
